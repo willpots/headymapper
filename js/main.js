812b709f3848f82cc99f328d96c04469bf7a9030
Will Potter <willstonepotter@gmail.com>
2015-01-20 10:38:54 -0500

    adding popups

--- js/main.js
+++ js/main.js
@@ -2,13 +2,15 @@ L.mapbox.accessToken = 'pk.eyJ1Ijoid2lsbHBvdHMiLCJhIjoiSTJYS0RCNCJ9.jPqwSxzqRHyj
 var map = L.mapbox.map('map', 'mapbox.light', { zoomControl: false })
   .setView([44.07377376789347, -72.79678344726562], 9);
 var zoom = new L.Control.Zoom({ position: 'topright' }).addTo(map);
-
+var locations;
+var myLocation;
 $.ajax({
   method: "GET",
   url: "locations.geojson",
   dataType: "json"
 })
 .done(function(geojson) {
+  locations = geojson;
   geojson.features = geojson.features.filter(function(feature) {
     console.log(feature);
     return feature.properties.type === "store";
@@ -18,7 +20,8 @@ $.ajax({
   });
   var geoJsonLayer = L.geoJson(geojson, {
     pointToLayer: function(feature, latlng) {
-      return L.marker(latlng, feature.properties.markerOptions);
+      return L.marker(latlng, feature.properties.markerOptions)
+        .bindPopup(feature.properties.name + "<br>" + feature.properties.address);
     }
   }).on('layeradd', function(e) {
     var marker = e.layer,
@@ -27,7 +30,19 @@ $.ajax({
     marker.setIcon(L.icon(feature.properties.icon));
   }).addTo(map);
 });
+enableGeolocation();
+function enableGeolocation() {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log(position.coords.latitude, position.coords.longitude);
+      myLocation = [position.coords.latitude, position.coords.longitude];
+      L.marker([position.coords.latitude, position.coords.longitude], {}).addTo(map);
+    });
 
+  } else {
+    /* geolocation IS NOT available */
+  }
+}
 function styleProperties(properties) {
   var icon = "";
   var day;
